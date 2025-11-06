37190c1575de61f143fb68331d28a5b46f7b4387
lululthfh <punyalulu05@gmail.com>
2025-11-06 19:43:52 +0700

    feat: menambahkan 3 composable yakni Home, TampilanInfo, dan FormIsi di mana Home memanggil onSubmitBtnClick untuk mengarah kepada halaman Detail, TampilanInfo memanggil onBerandaBtnClick yang mengarah pada halaman sebelumnya dan onFormBtnClick yang mengarah pada halaman Formulir, dan FormIsi memanggil onBackBtnClick yang mengarah pada halaman Detail

--- app/src/main/java/com/example/tugas_prak5/Navigasi.kt
+++ app/src/main/java/com/example/tugas_prak5/Navigasi.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi{
@@ -21,10 +21,31 @@ fun AppUlsSederhana(
     modifier: Modifier
 ){
     Scaffold { iniRuangan->
-        NavHost(
+        NavHost (
             navController = navController,
             startDestination= Navigasi.LandingPage.name,
             modifier = Modifier.padding(paddingValues = iniRuangan)
-        )
+        ){
+            composable(route = Navigasi.LandingPage.name){
+                Home(
+                    onSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilanInfo(
+                    onBerandaBtnClick = { cancelAndBackToFormulir(navController) },
+                    onFormBtnClick = {
+                        navController.navigate(route = Navigasi.Formulir.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                FormIsi(
+                    onBackBtnClick = navController.navigate(route = Navigasi.Detail.name)
+                )
+            }
+        }
     }
 }
