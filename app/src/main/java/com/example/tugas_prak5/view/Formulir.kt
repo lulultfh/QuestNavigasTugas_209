9055b7634150e125560d0e5bda66438528cce347
lululthfh <punyalulu05@gmail.com>
2025-11-06 19:55:02 +0700

    chore: menambahkan @OptIn untuk penggunaan API Material3 dalam Formulir.kt

--- app/src/main/java/com/example/tugas_prak5/view/Formulir.kt
+++ app/src/main/java/com/example/tugas_prak5/view/Formulir.kt
@@ -1,2 +1,7 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.tugas_prak5.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+@OptIn(ExperimentalMaterial3Api::class)
