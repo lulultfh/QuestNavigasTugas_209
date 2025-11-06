1c77e6b3ac3ae0e28c29f22d63d0252ab632cf15
lululthfh <punyalulu05@gmail.com>
2025-11-06 19:57:55 +0700

    feat: menambahkan composable function bernamakan Home yang memiliki parameter onSubmitBtnClick pada Home.kt

--- app/src/main/java/com/example/tugas_prak5/view/Home.kt
+++ app/src/main/java/com/example/tugas_prak5/view/Home.kt
@@ -2,5 +2,10 @@
 package com.example.tugas_prak5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Home(
+    onSubmitBtnClick: () -> Unit
+){}
