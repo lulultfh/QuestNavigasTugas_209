8f06aa400f832be7afc20d8a141f4c16d40985c6
lululthfh <punyalulu05@gmail.com>
2025-11-06 19:21:25 +0700

    feat: membuat composable function bernamakan AppUlsSederhana yang memiliki parameter navController untuk mengatur navigasi dan modifier

--- app/src/main/java/com/example/tugas_prak5/Navigasi.kt
+++ app/src/main/java/com/example/tugas_prak5/Navigasi.kt
@@ -1,7 +1,19 @@
 package com.example.tugas_prak5
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi{
     LandingPage,
     Detail,
     Formulir
 }
+
+@Composable
+fun AppUlsSederhana(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){}
