e251343ff669087e819cef260a3ab55e6599980a
lululthfh <punyalulu05@gmail.com>
2025-11-06 19:54:31 +0700

    chore: menambahkan @OptIn untuk penggunaan API Material3 dalam Home.kt

--- app/src/main/java/com/example/tugas_prak5/view/Home.kt
+++ app/src/main/java/com/example/tugas_prak5/view/Home.kt
@@ -1,2 +1,6 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.tugas_prak5.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+@OptIn(ExperimentalMaterial3Api::class)
