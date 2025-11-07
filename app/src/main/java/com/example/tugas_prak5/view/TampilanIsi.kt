1d14ccbfb222546ea47dc197212e7bc72cb257fa
lululthfh <punyalulu05@gmail.com>
2025-11-07 12:27:16 +0700

    feat: menambahkan composable function bernamakan TampilanInfo yang memiliki parameter onBerandaBtnClick dan onFormBtnClick pada TampilanIsi.kt

--- app/src/main/java/com/example/tugas_prak5/view/TampilanIsi.kt
+++ app/src/main/java/com/example/tugas_prak5/view/TampilanIsi.kt
@@ -2,5 +2,12 @@
 package com.example.tugas_prak5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampianInfo(
+    onBerandaBtnClick: ()-> Unit,
+    onFormBtnCLick:() -> Unit
+){}
