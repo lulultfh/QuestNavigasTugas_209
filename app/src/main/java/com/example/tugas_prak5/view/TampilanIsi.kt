ce7090db5e8ba779fde795251e9851fbbc81398f
lululthfh <punyalulu05@gmail.com>
2025-11-07 12:43:25 +0700

    feat: menambahkan Card dan button component yang masing-masing dipisahkan oleh Spacer dalam Column pada body Scaffold dalam function TampilanInfo

--- app/src/main/java/com/example/tugas_prak5/view/TampilanIsi.kt
+++ app/src/main/java/com/example/tugas_prak5/view/TampilanIsi.kt
@@ -1,6 +1,16 @@
 @file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.tugas_prak5.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -10,12 +20,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tugas_prak5.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TampianInfo(
+fun TampilanInfo(
     onBerandaBtnClick: ()-> Unit,
     onFormBtnCLick:() -> Unit
 ){
@@ -31,5 +46,35 @@ fun TampianInfo(
                 title = { Text(text = stringResource(id = R.string.listnyaKak), color = Color.White) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.purple_200))
             )
-        }) {  }
+        }) { isiRuang->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(space = dimensionResource(id = R.dimen.padding_small))) {
+                items.forEach { item ->
+                    Column {
+                        Card(colors = CardDefaults.cardColors(containerColor = Color.White),
+                            modifier = Modifier
+                                .fillMaxSize()) {
+                            Text(text = item.first.uppercase(),
+                                fontSize = 16.sp)
+                            Text(text = item.second,
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(height = 10.dp))
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = onBerandaBtnClick
+                    ) { Text(text = stringResource(id = R.string.beranda)) }
+                    Spacer(modifier = Modifier.height(height = 10.dp))
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = onFormBtnCLick
+                    ) { Text(text = stringResource(id = R.string.form_daftar)) }
+                }
+            }
+        }
+    }
 }
