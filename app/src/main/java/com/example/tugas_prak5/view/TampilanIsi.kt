2763d2bf3f73cbdf6de46e4e5e9fcadcc328c293
lululthfh <punyalulu05@gmail.com>
2025-11-07 12:32:06 +0700

    feat: menambahkan Scaffold di dalam function TampilData dengan parameter topBar yang menggunakan TopAppBar dengan title mengambil stringResource dari id listnyaKak, dan mengambil colorResource untuk warna topAppBar

--- app/src/main/java/com/example/tugas_prak5/view/TampilanIsi.kt
+++ app/src/main/java/com/example/tugas_prak5/view/TampilanIsi.kt
@@ -2,7 +2,14 @@
 package com.example.tugas_prak5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.tugas_prak5.R
 
@@ -18,4 +25,11 @@ fun TampianInfo(
         Pair(first = stringResource(id = R.string.statuskawin), second = "Lajang"),
         Pair(first = stringResource(id = R.string.alamat), second = "Sleman")
     )
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.listnyaKak), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.purple_200))
+            )
+        }) {  }
 }
