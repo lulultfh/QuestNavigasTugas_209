8cb21dc8c80a4766d9be905443ba2e096a1eae34
lululthfh <punyalulu05@gmail.com>
2025-11-07 12:30:25 +0700

    feat: menambahkan variable read-only bernamakan items yang bertipekan list yang memanggil string resource dengan id nama_lengkap, jenis_kelamin, statuskawin dan alamat

--- app/src/main/java/com/example/tugas_prak5/view/TampilanIsi.kt
+++ app/src/main/java/com/example/tugas_prak5/view/TampilanIsi.kt
@@ -3,11 +3,19 @@ package com.example.tugas_prak5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.compose.ui.res.stringResource
+import com.example.tugas_prak5.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampianInfo(
     onBerandaBtnClick: ()-> Unit,
     onFormBtnCLick:() -> Unit
-){}
+){
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Aprilia Kurnianti"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Perempuan"),
+        Pair(first = stringResource(id = R.string.statuskawin), second = "Lajang"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Sleman")
+    )
+}
