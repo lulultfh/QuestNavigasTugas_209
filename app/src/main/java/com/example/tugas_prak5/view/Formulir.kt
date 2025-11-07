42c5e2ab458cace770cd209860828f8896492bd9
lululthfh <punyalulu05@gmail.com>
2025-11-07 12:48:19 +0700

    feat: menambahkan Composable function bernamakan FormIsi dengan parameter jenisK untuk menampilkan list Laki-laki dan Perempuan, dan OnBackBtnClick sebagai function yang dijalankan ketika Submit di klik

--- app/src/main/java/com/example/tugas_prak5/view/Formulir.kt
+++ app/src/main/java/com/example/tugas_prak5/view/Formulir.kt
@@ -3,5 +3,11 @@
 package com.example.tugas_prak5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsi(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    onBackBtnClick : () -> Unit
+){}
