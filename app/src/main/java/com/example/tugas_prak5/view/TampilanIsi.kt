b2d80e03f85fef70b26b93ba9ac46bc27e49da1a
lululthfh <punyalulu05@gmail.com>
2025-11-07 13:32:18 +0700

    refactor: memindahkan button Beranda dan Form Daftar keluar dari loop item serta memperbarui style TampilanInfo dengan layout dan warna baru

--- app/src/main/java/com/example/tugas_prak5/view/TampilanIsi.kt
+++ app/src/main/java/com/example/tugas_prak5/view/TampilanIsi.kt
@@ -1,6 +1,7 @@
 @file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.tugas_prak5.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -43,37 +46,52 @@ fun TampilanInfo(
     Scaffold(modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(id = R.string.listnyaKak), color = Color.White) },
+                title = { Text(text = stringResource(id = R.string.listnyaKak), color = Color.White, fontSize = 30.sp) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.purple_200))
             )
         }) { isiRuang->
-        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+        Column(modifier = Modifier
+            .padding(paddingValues = isiRuang)
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.unguagakputih)),
             verticalArrangement = Arrangement.SpaceBetween) {
             Column(modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(space = dimensionResource(id = R.dimen.padding_small))) {
+                verticalArrangement = Arrangement.Top) {
                 items.forEach { item ->
-                    Column {
-                        Card(colors = CardDefaults.cardColors(containerColor = Color.White),
-                            modifier = Modifier
-                                .fillMaxSize()) {
+                    Card(elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                        colors = CardDefaults.cardColors(containerColor = Color.White),
+                        modifier = Modifier
+                            .fillMaxWidth()) {
+                        Column(modifier = Modifier
+                            .padding(all=15.dp)) {
                             Text(text = item.first.uppercase(),
-                                fontSize = 16.sp)
+                                fontSize = 16.sp,
+                                color = Color.Black)
+                            Spacer(modifier = Modifier.height(5.dp))
                             Text(text = item.second,
                                 fontWeight = FontWeight.Bold,
-                                fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                                fontFamily = FontFamily.Cursive, fontSize = 22.sp,
+                                color = Color.Black)
                         }
                     }
                     Spacer(modifier = Modifier.height(height = 10.dp))
-                    Button(
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = onBerandaBtnClick
-                    ) { Text(text = stringResource(id = R.string.beranda)) }
-                    Spacer(modifier = Modifier.height(height = 10.dp))
-                    Button(
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = onFormBtnClick
-                    ) { Text(text = stringResource(id = R.string.form_daftar)) }
                 }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBerandaBtnClick,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.ungutua)
+                    )
+                ) { Text(text = stringResource(id = R.string.beranda)) }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onFormBtnClick,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.ungumuda)
+                    )
+                ) { Text(text = stringResource(id = R.string.form_daftar)) }
             }
         }
     }
